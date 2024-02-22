1f69950dd91d183e4c8111664ac101a5fb211353
AaronnF <114843419+AaronnF@users.noreply.github.com>
2024-02-22 22:50:46 +0800

    Add files via upload

---
+++ nestling copy/src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import LogIn from './components/LogIn.jsx';
+
+function App() {
+  return (
+    <div>
+        <LogIn />
+    </div>
+  );
+}
+
+export default App;
---
+++ nestling copy/src/components/LogIn.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function LoginPage() {
+    return (
+        <div>
+            <h1 className="text-center">Nestling.io</h1>
+        </div>
+    );
+  }
+
+export default LoginPage;
