de235e1ba4b1c079986400410aef362b55207b07
AaronnF <aaronfernandez715@gmail.com>
2024-03-02 16:01:49 +0800

    updated the landing page
    removed logo from landing page

--- nestling copy/src/components/LandingPage.jsx
+++ nestling copy/src/components/LandingPage.jsx
@@ -1,13 +1,11 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import walter from '../../src/assets/images/walter.png';
 import interiordesign from '../../src/assets/images/interiordesign.png';
 import './LandingPage.css'; // Import the CSS file for styling
 
 function LandingPage() {
     return (
         <div className="landing-page" style={{backgroundImage: `url(${interiordesign})`}}>
-            <div><img src={walter} alt="Walter" /></div>
             <div><h1>nestling.ai</h1></div>
             <div><h2>Unlock the door to your future with a simple click.</h2></div>
             <div className="button-container">
