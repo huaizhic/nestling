92ed53bf5b003cdc357e840fddba6daa8e14f3d1
AaronnF <aaronfernandez715@gmail.com>
2024-03-15 14:01:28 +0800

    fixed acctdetails
    fixed margin issue on account details page

--- nestling copy/src/components/AcctDetails.jsx
+++ nestling copy/src/components/AcctDetails.jsx
@@ -25,47 +25,42 @@ function AcctDetails() {
   return (
     <div className="acct-details">
       <div className="logo"><img src={walterlogo} alt="Walter Logo" /></div>
-      <div className="profile-picture"><img src={greenwalter} alt="Green Walter Profile" /></div>
       <div className="user-info">
-            <div className="input-wrapper">
-            <label htmlFor="username">Username</label>
-            <input
-                type="text"
-                id="username"
-                value={username}
-                onChange={(e) => setUsername(e.target.value)}
-            />
-            </div>
-            <div className="input-wrapper">
-            <label htmlFor="email">Email</label>
-            <input
-                type="text"
-                id="email"
-                value={email}
-                onChange={(e) => setEmail(e.target.value)}
-            />
-            </div>
-            <div className="input-wrapper">
-            <label htmlFor="password">Password</label>
-            <input
-                type="text"
-                id="password"
-                value={password}
-                onChange={(e) => setPassword(e.target.value)}
-            />
-            </div>
-            <div className="links-wrapper">
-            <div className="link-wrapper-1"> 
-                <Link to="/change-password">Change Password</Link>
-            </div>
-            <div className="link-wrapper-2">
-                <Link to="/change-profile-picture">Change Profile Picture</Link>
-            </div>
-            </div>
+        <div className="profile-pic"><img src={greenwalter} alt="Green Walter Profile" /></div>
+        <div className="input-wrapper">
+        <label htmlFor="username">Username</label>
+        <input
+            type="text"
+            id="username"
+            value={username}
+            onChange={(e) => setUsername(e.target.value)}
+        />
         </div>
-      <div className="button">
-      <button>Fly</button>
+        <div className="input-wrapper">
+        <label htmlFor="email">Email</label>
+        <input
+            type="text"
+            id="email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+        />
+        </div>
+        <div className="input-wrapper">
+        <label htmlFor="password">Password</label>
+        <input
+            type="text"
+            id="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+        />
+        </div>
+        <div className="links-wrapper">
+        <div className="link-wrapper-1"> 
+            <Link to="/change-password">Change Password</Link>
+        </div>
+        </div>
       </div>
+      <div className="button"><button>Fly</button></div>
     </div>
   );
 }
--- nestling copy/src/components/CurrentListings.jsx
+++ nestling copy/src/components/CurrentListings.jsx
@@ -9,23 +9,23 @@ function CurrentListings(){
         <div className="current-listings">
             <div className="topcontainer">
                 <div className="logo"><img src={walterlogo} alt="Walter Logo" /></div>
-                <div className="profile-picture"><img src={greenwalter} alt="Green Walter Profile" /></div>
                 <div className="navbar">
-                <ul>
-                    <li>
-                        <Link to="/">Home</Link>
-                    </li>
-                    <li>
-                        <Link to="/desired-property">Desired Property</Link>
-                    </li>
-                    <li>
-                        <Link to="/current-listings">Current Listings</Link>
-                    </li>
-                    <li>
-                        <Link to="/favourites">Favourites</Link>
-                    </li>
-                </ul>
-            </div>
+                    <ul>
+                        <li>
+                            <Link to="/">Home</Link>
+                        </li>
+                        <li>
+                            <Link to="/desired-property">Desired Property</Link>
+                        </li>
+                        <li>
+                            <Link to="/current-listings">Current Listings</Link>
+                        </li>
+                        <li>
+                            <Link to="/favourites">Favourites</Link>
+                        </li>
+                    </ul>
+                </div>
+                <div className="profile-picture"><img src={greenwalter} alt="Green Walter Profile" /></div>
             </div>
         </div>
   );
