de29c17ce9775bd8271be4964c30ec1cfd41836f
huaizhic <huaizhihuaizhi@gmail.com>
2024-03-27 17:53:17 +0800

    Refine Application skeleton for Lab 3

--- Lab3/Application Skeleton/LandingPage.jsx
+++ Lab3/Application Skeleton/LandingPage.jsx
@@ -1,20 +1,11 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
-import indoorpool from '../assets/images/indoorpool.png';
-import './LandingPage.css'; // Import the CSS file for styling
+import React from "react";
+import { Link } from "react-router-dom";
+import indoorpool from "../assets/images/indoorpool.png";
+import "./LandingPage.css"; // Import the CSS file for styling
 
 function LandingPage() {
-    return (
-        <div className="landing-page" style={{backgroundImage: `url(${indoorpool})`}}>
-            <div><h1>nestling.ai</h1></div>
-            <div><h2>Unlock the door to your<br/>future with a simple click.</h2></div>
-            <div className="button-container">
-                <Link to="/login"><button>Login</button></Link>
-                <Link to="/sign-up"><button>Signup</button></Link>
-            </div>
-        </div>
-    );
+  // component for landing page, the first page users see when they first enter the web app
+  // responsible for onboarding (sign in, sign up)
 }
 
 export default LandingPage;
-
---
+++ Lab3/Application Skeleton/Navbar.jsx
@@ -0,0 +1,4 @@
+export function NavBar() {
+  // navigation bar component to be stickied at the top of the website for most pages,
+  // for user ease of access to different pages
+}
