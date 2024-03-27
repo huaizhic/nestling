7c85e4f1ec817162d4e2a6f29dbebec2ec3c6999
AaronnF <aaronfernandez715@gmail.com>
2024-03-28 00:52:03 +0800

    partially fixed current listings

--- nestling/src/components/ListingPanel.jsx
+++ nestling/src/components/ListingPanel.jsx
@@ -6,9 +6,9 @@ export const ListingPanel = ({ title, price, percentageMatch }) => {
 
   return (
     <>
-      <Link to="/listing-info" style={{ textDecoration: 'none' }}>
       <div className="listingPanel">
         <div className="panelWrapper">
+        <Link to="/listing-info" style={{ textDecoration: 'none' }}>
           <div className="listingPanelImage"><img src={emptyimage}></img></div>
           <h3>{title}</h3>
           <h3>${price}</h3>
@@ -18,13 +18,13 @@ export const ListingPanel = ({ title, price, percentageMatch }) => {
               ? "Search to find out"
               : percentageMatch + "%"}
             </span>*/}
-          <span className={percentageMatch !== undefined ? "percentageBubble greenBubble" : "percentageBubble"}>
+          <span className={`percentageBubble ${percentageMatch !== undefined ? "greenBubble" : ""}`}>
             <div className="value">{percentageMatch !== undefined && Math.round(percentageMatch) + "%"}</div>
             <div className="match">{percentageMatch !== undefined && "match"}</div>
           </span>
+          </Link>
         </div>
       </div>
-      </Link>
     </>
   );
 };
