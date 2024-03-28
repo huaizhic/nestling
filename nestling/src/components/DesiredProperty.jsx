765e820f91b34a69edc4b5175ab657b7b7cc619e
pranavipea <pranavi11panduga@gmail.com>
2024-03-28 22:50:35 +0800

    desired property functionality done!

--- nestling/src/components/DesiredProperty.jsx
+++ nestling/src/components/DesiredProperty.jsx
@@ -1,18 +1,7 @@
 import React, { useState, useEffect } from "react";
-import {Link  } from "react-router-dom";
 import "./DesiredProperty.css";
-import ListingInfo from "./ListingInfo";
 import Dropdown from './Dropdown.jsx'
-
-// import "./CurrentListings.css";
-import walterlogo from "../../src/assets/images/walterlogo.png";
-import greenwalter from "../../src/assets/images/greenwalter.png";
-import { ListingPanel } from "./ListingPanel";
 import supabase from "../supabase";
-import { percentageMatchLogic } from "./percentageMatchLogic";
-// import { tempData } from "./desiredHouseForm";
-
-// export let currentList = [];
 
 export let desiredAttributes = [
   {
@@ -28,66 +17,58 @@ export let desiredAttributes = [
 
 function DesiredProperty() {
   const [currentList, setCurrentList] = useState([]);
-  const [fetchflag, setFetchFlag] = useState(false);
   const [selectedLocation, setLocation] = useState('');
-  const [selectedDistance, setDistance] = useState('');
+  const [selectedDistance, setDistance] = useState('a');
   const [selectedRoomCount, setRoomCount] = useState('');
   const [selectedGFA, setGFA] = useState('');
   const [selectedAmenities1, setAmenities1] = useState('');
   const [selectedAmenities2, setAmenities2] = useState('');
-  const [selectedAmenities3, setAmenities3] = useState(1500);
+  const [selectedAmenities3, setAmenities3] = useState('');
+  const [isVisible, setIsVisible] = useState(false);
 
   useEffect(() => {
-    // const fetchUser = async () => {
-    //   const {
-    //     data: { user },
-    //   } = await supabase.auth.getUser();
-    //   console.log(user);
-    // };
+    const fetchData = async () => {
+      const { data, error } = await supabase.from("currentList").select("*");
+      if (error) {
+        console.error("Error fetching data:", error.message);
+      } else {
+        setCurrentList(data);
+      }
 
-  const fetchListing = async () => {
-    let { data, error } = await supabase.from("currentList").select("*");
-    //   console.log(data);
-    setCurrentList(data);
+      const { data: userData, error: userError } = await supabase.auth.getUser();
+      if (userError) {
+        console.error("Error fetching user data:", userError.message);
+      } else {
+        const { data: userDesiredData, error: desiredError } = await supabase
+          .from("userInfo")
+          .select("desiredProperty")
+          .eq("email", userData.email);
+
+        if (desiredError) {
+          console.error("Error fetching desired property data:", desiredError.message);
+        } else {
+          const fetchedDesiredAttributes = userDesiredData[0]?.desiredProperty[0];
+          if (fetchedDesiredAttributes) {
+            setLocation(fetchedDesiredAttributes.location || '');
+            setAmenities1(fetchedDesiredAttributes.amenity1 || '');
+            setAmenities2(fetchedDesiredAttributes.amenity2 || '');
+            setAmenities3(fetchedDesiredAttributes.amenity3 || '');
+            setDistance(fetchedDesiredAttributes.distanceRadius || '');
+            setRoomCount(fetchedDesiredAttributes.roomCount || '');
+            setGFA(fetchedDesiredAttributes.grossFloorArea || '');
+          }
+        }
+      }
+    };
+    fetchData();
+  }, []);
+
+  const toggleVisibility = () => {
+    setIsVisible(!isVisible);
   };
-
-  // due to duplicates in location options
-  const filterLocationList = () => {};
-
-  const fetchDesiredAtrributes = async () => {
-    const {
-      data: { user },
-    } = await supabase.auth.getUser();
-    console.log(user);
-
-    let { data, error } = await supabase
-      .from("userInfo")
-      .select("desiredProperty")
-      .eq("email", user.email);
-    //   console.log(data);
-    console.log(data[0].desiredProperty[0]);
-    let fetchedDesiredAttributes = data[0].desiredProperty[0];
-
-    setLocation(fetchedDesiredAttributes.location);
-    setAmenities1(fetchedDesiredAttributes.amenity1);
-    setAmenities2(fetchedDesiredAttributes.amenity2);
-    setAmenities3(fetchedDesiredAttributes.amenity3);
-    setDistance(fetchedDesiredAttributes.distanceRadius);
-    setRoomCount(fetchedDesiredAttributes.roomCount);
-    setGFA(fetchedDesiredAttributes.grossFloorArea);
-  };
-  fetchListing();
-  fetchDesiredAtrributes();
-  }, [fetchflag]);
-
-  if (currentList.length === 0) {
-    // setFetchFlag(!fetchflag);
-    return <h1>Fetching data...</h1>;
-  }
 
   const handleSave = async (e) => {
     e.preventDefault();
-
     desiredAttributes[0].location = selectedLocation;
     desiredAttributes[0].amenity1 = selectedAmenities1;
     desiredAttributes[0].amenity2 = selectedAmenities2;
@@ -95,32 +76,54 @@ function DesiredProperty() {
     desiredAttributes[0].distanceRadius = selectedDistance;
     desiredAttributes[0].roomCount = selectedRoomCount;
     desiredAttributes[0].grossFloorArea = selectedGFA;
-    console.log(desiredAttributes);
 
-    const {
-      data: { user },
-    } = await supabase.auth.getUser();
-    console.log(user);
+    const { data: userData, error: userError } = await supabase.auth.getUser();
+    if (userError) {
+      console.error("Error fetching user data:", userError.message);
+      return;
+    }
 
     const { data, error } = await supabase
       .from("userInfo")
       .update({ desiredProperty: desiredAttributes })
-      .eq("email", user.email)
+      .eq("email", userData.email)
       .select();
 
-    console.log(data);
-    console.log(error);
-
-    if (data) {
-      alert("update success!");
-    } else if (error) {
-      alert("error: ", error);
+    if (error) {
+      console.error("Error updating desired property:", error.message);
     } else {
-      alert("there was a problem updating");
+      alert("Update success!");
     }
   };
 
-  //for dropdown boxes: amenities, room count and distance
+  //for dropdown boxes: amenities, room count, distance and location
+  const locations = [
+    {value: 'Ang Mo Kio', label: 'Ang Mo Kio'},
+    {value: 'Bedok', label: 'Bedok'},
+    {value: 'Bishan', label: 'Bishan'}, 
+    {value: 'Bukit Batok', label: 'Bukit Batok'},
+    {value: 'Bukit Merah', label: 'Bukit Merah'},
+    {value: 'Bukit Panjang', label: 'Bukit Panjang'},
+    {value: 'Choa Chu Kang', label: 'Choa Chu Kang'}, 
+    {value: 'Clementi', label: 'Clementi'},
+    {value: 'Geylang', label: 'Geylang'},
+    {value: 'Hougang', label: 'Hougang'},
+    {value: 'Jurong East', label: 'Jurong East'},
+    {value: 'Jurong West', label: 'Jurong West'},
+    {value: 'Kallang/Whampoa', label: 'Kallang/Whampoa'},
+    {value: 'Pasir Ris', label: 'Pasir Ris'},
+    {value: 'Punggol', label: 'Punggol'},
+    {value: 'Queenstown', label: 'Queenstown'},
+    {value: 'Sembawang', label: 'Sembawang'},
+    {value: 'Sengkang', label: 'Sengkang'},
+    {value: 'Serangoon', label: 'Serangoon'},
+    {value: 'Tampines', label: 'Tampines'},
+    {value: 'Tengah', label: 'Tengah'},
+    {value: 'Toa Payoh', label: 'Toa Payoh'},
+    {value: 'Woodlands', label: 'Woodlands'},
+    {value: 'Yishun', label: 'Yishun'},
+];
+
   const distance = [
     {value: '0-1km', label: '0-1km'},
     {value: '1-2km', label: '1-2km'},
@@ -145,168 +148,103 @@ function DesiredProperty() {
   {value: 'Supermarkets', label:'Supermarkets'},
   ];
 
-  //console logging a json file
-  const generateJson = async (e) => {
-
-    desiredAttributes[0].location = selectedLocation;
-    desiredAttributes[0].amenity1 = selectedAmenities1;
-    desiredAttributes[0].amenity2 = selectedAmenities2;
-    desiredAttributes[0].amenity3 = selectedAmenities3;
-    desiredAttributes[0].distanceRadius = selectedDistance;
-    desiredAttributes[0].roomCount = selectedRoomCount;
-    desiredAttributes[0].grossFloorArea = selectedGFA;
-    console.log(desiredAttributes);
-
-    const {
-      data: { user },
-    } = await supabase.auth.getUser();
-    console.log(user);
-    
-    const { data, error } = await supabase
-    .from("userInfo")
-    .update({ desiredProperty: desiredAttributes })
-    .eq("email", user.email)
-    .select();
-
-
-
-    console.log(data);
-    console.log(error);
-  }
-
-    return(
-        <div className="desired-property">
-            <div className="top">
-              <ListingInfo />
-            </div>
-            <div className="bottom">
-                <div className="header-section">
-                    <h2 className="header">Desired Property</h2>
-                </div>
-                <div className="dropdown-div">
-                  <div className="location-div">
-                    <h3 className="location">Location</h3>
-                  </div>
-                  <select
-                    id="Location"
-                    value={selectedLocation}
-                    className="dropdown"
-                    onChange={(e) => setLocation(e.target.value)}
-                  >
-                    <option value="option1">Preferred location</option>
-                    {/* <option value="option2">Woodlands</option>
-                    <option value="option3">Tiong Bahru</option>
-                    <option value="option4">Tanjong Pagar</option>
-                    <option value="option5">Orchard</option> */}
-                    {currentList.map((indivPanel) => {
-                      // let isDuplicate = currentList.some(
-                      //   (property) => property.nearestMRT === indivPanel.nearestMRT
-                      // );
-                      return (
-                        <option value={indivPanel.nearestMRT}>
-                          {indivPanel.nearestMRT}
-                        </option>
-                      );
-                    })}
-                  </select>
-                </div>
-                <div className="amenities">
-                    <div>
-                        <h3 className="amenities-div">Amenities</h3>
-                    </div>
-                    <div className="amenities-dropdowns">
-                        <div className="amenities-1">
-                          <Dropdown
-                            options={amenities}
-                            selectedOption={selectedAmenities1}
-                            onSelect={setAmenities1}
-                            onChange={(e) => {
-                              if (
-                                e.target.value === selectedAmenities2 ||
-                                e.target.value === selectedAmenities3
-                              ) {
-                                alert("Cannot choose same amenity again");
-                                setAmenities1("option1");
-                              } else {
-                                setAmenities1(e.target.value);
-                              }
-                            }}
-                          />
-                        </div>
-                        <div className="amenities-2">
-                          <Dropdown
-                            options={amenities}
-                            selectedOption={selectedAmenities2}
-                            onSelect={setAmenities2}
-                            onChange={(e) => {
-                              if (
-                                e.target.value === selectedAmenities1 ||
-                                e.target.value === selectedAmenities3
-                              ) {
-                                alert("Cannot choose same amenity again");
-                                setAmenities2("option2");
-                              } else {
-                                setAmenities2(e.target.value);
-                              }
-                            }}
-                          />
-                        </div>
-                        <div className="amenities-3">
-                          <Dropdown
-                            options={amenities}
-                            selectedOption={selectedAmenities3}
-                            onSelect={setAmenities3}
-                            onChange={(e) => {
-                              if (
-                                e.target.value === selectedAmenities2 ||
-                                e.target.value === selectedAmenities1
-                              ) {
-                                alert("Cannot choose same amenity again");
-                                setAmenities3("option3");
-                              } else {
-                                setAmenities3(e.target.value);
-                              }
-                            }}
-                          />
-                        </div>
-                    </div>
-                </div>
-                <div className="dropdown-div">
-                  <h3 className="distance">Distance</h3>
-                  <Dropdown
-                    options={distance}
-                    selectedOption={selectedDistance}
-                    onSelect={setDistance}
-                  />
-                </div>
-
-                <div className="dropdown-div">
-                    <h3 className ="room-count">Room Count</h3>
-                    <Dropdown
-                        options={roomcount}
-                        selectedOption={selectedRoomCount}
-                        onSelect={setRoomCount}
-                    />
-                </div>
-
-                <div className="dropdown-div">
-                  <h3 className ="gfa">GFA</h3>
-                  <input
-                    type="number"
-                    value={selectedGFA}
-                    className="dropdown"
-                    onChange={(e) => setGFA(e.target.value)}
-                  />
-                </div>
-            </div>
-
-            <div className="generate-price">
-              <Link to ="/generated-price">
-                <h2 onClick={() => generateJson()}>Generate Price</h2>
-              </Link>
-            </div>
+  return (
+    <div className="desired-property">
+      <div className="top"></div>
+      <div className="bottom">
+        <div className="header-section">
+          <h2 className="header">Desired Property</h2>
         </div>
-    );
+        <div className="dropdown-div">
+          <h3 className="location">Location</h3>
+            <Dropdown 
+              options={locations}
+              selectedOption={selectedLocation}
+              onSelect={setLocation}
+            />
+        </div>
+
+        <div className="amenities">
+          <div>
+            <h3 className="amenities-div">Amenities</h3>
+          </div>
+          <div className="amenities-dropdowns">
+            <div className="amenities-1">
+              <Dropdown
+                options={amenities}
+                selectedOption={selectedAmenities1}
+                onSelect={setAmenities1}
+              />
+            </div>
+            <div className="amenities-2">
+              <Dropdown
+                options={amenities}
+                selectedOption={selectedAmenities2}
+                onSelect={setAmenities2}
+              />
+            </div>
+            <div className="amenities-3">
+              <Dropdown
+                options={amenities}
+                selectedOption={selectedAmenities3}
+                onSelect={setAmenities3}
+              />
+            </div>
+          </div>
+        </div>
+        <div className="dropdown-div">
+          <h3 className="distance">Distance</h3>
+          <Dropdown
+            options={distance}
+            selectedOption={selectedDistance}
+            onSelect={setDistance}
+          />
+        </div>
+
+        <div className="dropdown-div">
+          <h3 className ="room-count">Room Count</h3>
+          <Dropdown
+            options={roomcount}
+            selectedOption={selectedRoomCount}
+            onSelect={setRoomCount}
+          />
+        </div>
+
+        <div className="dropdown-div">
+          <h3 className ="gfa">GFA</h3>
+          <input
+            type="number"
+            value={selectedGFA}
+            className="dropdown"
+            onChange={(e) => setGFA(e.target.value)}
+          />
+        </div>
+      </div>
+        {!isVisible && (
+          <div>
+            <button onClick={toggleVisibility} className="generate-price">Generate Price</button>
+          </div>
+        )}
+        {isVisible && (
+          <div className="after-click">
+            <div className="generated-price-div">
+              <p className="generated-price-header">Estimated Price</p>
+              <div className="estimated-price-div">
+                <p className="estimated-price">~$100k</p>
+              </div>
+            </div>
+            <div className="buttons">
+              <div>
+                <button className="reconfigure">Reconfigure</button>
+              </div>
+              <div>
+                <button className="confirm">Confirm</button>
+              </div>
+            </div>
+          </div>
+        )}
+      </div>
+  );
 }
 
 export default DesiredProperty;
