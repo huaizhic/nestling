98e2e7f641028c634d491744a876a60ae0c628e5
AaronnF <aaronfernandez715@gmail.com>
2024-04-05 15:31:20 +0800

    Update ListingDetails.jsx

--- nestling/src/components/ListingDetails.jsx
+++ nestling/src/components/ListingDetails.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Link, useHistory } from "react-router-dom";
+import { Link } from "react-router-dom";
 import walterlogo from "../../src/assets/images/walterlogo.png";
 import greenwalter from "../../src/assets/images/greenwalter.png";
 import "./ListingDetails.css";
