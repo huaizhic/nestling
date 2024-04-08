a79b5b1eb8ee2ce9d7574f3668e4593ffced1fa5
huaizhic <huaizhihuaizhi@gmail.com>
2024-04-08 19:52:53 +0800

    Update Home.jsx

--- Lab4/src/components/Home.jsx
+++ Lab4/src/components/Home.jsx
@@ -1,9 +1,10 @@
-import React, { useState, useEffect } from 'react';
-import { Link } from 'react-router-dom';
-import './Home.css';
-import './Navbar.css'
+import React, { useState, useEffect } from "react";
+import { Link } from "react-router-dom";
+import "./Home.css";
+import "./Navbar.css";
 import walterlogo from "../../src/assets/images/walterlogo.png";
 import greenwalter from "../../src/assets/images/greenwalter.png";
+import supabase from "../supabase";
 
 export default function Home() {
   async function handleLogout() {
@@ -20,11 +21,23 @@ export default function Home() {
         </div>
         <div className="navbar">
           <ul>
-            <li><Link to="/home">Home</Link></li>
-            <li><Link to="/desired-property">Desired Property</Link></li>
-            <li><Link to="/current-listings">Current Listings</Link></li>
-            <li><Link to="/favourites">Favourites</Link></li>
-            <li><Link to="/" onClick={handleLogout}>Logout</Link></li>
+            <li>
+              <Link to="/home">Home</Link>
+            </li>
+            <li>
+              <Link to="/desired-property">Desired Property</Link>
+            </li>
+            <li>
+              <Link to="/current-listings">Current Listings</Link>
+            </li>
+            <li>
+              <Link to="/favourites">Favourites</Link>
+            </li>
+            <li>
+              <Link to="/" onClick={handleLogout}>
+                Logout
+              </Link>
+            </li>
           </ul>
         </div>
         <div className="profile-picture">
@@ -39,32 +52,59 @@ export default function Home() {
               <h2 className="header-text">What's Chirping!</h2>
             </div>
             <div className="article-reg">
-              <div className ="article-content">
+              <div className="article-content">
                 <div>
-                  <Link to="https://blog.bluenest.sg/buying-private-property-procedure/" className="article-header">is the price truly right?</Link>
+                  <Link
+                    to="https://blog.bluenest.sg/buying-private-property-procedure/"
+                    className="article-header"
+                  >
+                    is the price truly right?
+                  </Link>
                 </div>
                 <div>
-                  <p className="article-content">read testimonials from previous clients on how they could find their dream home with just a simple search! and that too, for a reasonable price. read testimonials from previous clients on how they could find their dream home with just a simple search!</p>
+                  <p className="article-content">
+                    read testimonials from previous clients on how they could
+                    find their dream home with just a simple search! and that
+                    too, for a reasonable price. read testimonials from previous
+                    clients on how they could find their dream home with just a
+                    simple search!
+                  </p>
                 </div>
               </div>
             </div>
             <div className="article-reg">
-              <div className ="article-content">
+              <div className="article-content">
                 <div>
-                  <Link to="/article" className="article-header">is the price truly right?</Link>
+                  <Link to="/article" className="article-header">
+                    is the price truly right?
+                  </Link>
                 </div>
                 <div>
-                  <p className="article-content">read testimonials from previous clients on how they could find their dream home with just a simple search! and that too, for a reasonable price. read testimonials from previous clients on how they could find their dream home with just a simple search!</p>
+                  <p className="article-content">
+                    read testimonials from previous clients on how they could
+                    find their dream home with just a simple search! and that
+                    too, for a reasonable price. read testimonials from previous
+                    clients on how they could find their dream home with just a
+                    simple search!
+                  </p>
                 </div>
-                </div>
+              </div>
             </div>
             <div className="article-reg">
-              <div className ="article-content">
+              <div className="article-content">
                 <div>
-                  <Link to="/article" className="article-header">is the price truly right?</Link>
+                  <Link to="/article" className="article-header">
+                    is the price truly right?
+                  </Link>
                 </div>
                 <div>
-                  <p className="article-content">read testimonials from previous clients on how they could find their dream home with just a simple search! and that too, for a reasonable price. read testimonials from previous clients on how they could find their dream home with just a simple search!</p>
+                  <p className="article-content">
+                    read testimonials from previous clients on how they could
+                    find their dream home with just a simple search! and that
+                    too, for a reasonable price. read testimonials from previous
+                    clients on how they could find their dream home with just a
+                    simple search!
+                  </p>
                 </div>
               </div>
             </div>
@@ -76,7 +116,30 @@ export default function Home() {
             <h2 className="header-text">About us!</h2>
           </div>
           <div>
-            <p className="about-us-content">At Nestling, we believe in empowering individuals with the knowledge to make informed decisions about one of life's most significant investments – a home. Our platform specialises in extrapolating private housing prices and conducting meticulous comparisons to houses currently on the market.  Whether you are a prospective buyer, seller, or simply a curious observer, our commitment is to deliver accurate, up-to-date information to guide you on your journey through the world of real estate. Join us as we transform the way you perceive, assess, and navigate the housing market, making your homeownership dreams a well-informed reality. Welcome to Nestling – Where Homes and Insights Unite. At Nestling, we believe in empowering individuals with the knowledge to make informed decisions about one of life's most significant investments – a home. Our platform specialises in extrapolating private housing prices and conducting meticulous comparisons to houses currently on the market.  Whether you are a prospective buyer, seller, or simply a curious observer, our commitment is to deliver accurate, up-to-date information to guide you on your journey through the world of real estate. Join us as we transform the way you perceive, assess, and navigate the housing market, making your homeownership dreams a well-informed reality. Welcome to Nestling – Where Homes and Insights Unite.</p>
+            <p className="about-us-content">
+              At Nestling, we believe in empowering individuals with the
+              knowledge to make informed decisions about one of life's most
+              significant investments – a home. Our platform specialises in
+              extrapolating private housing prices and conducting meticulous
+              comparisons to houses currently on the market. Whether you are a
+              prospective buyer, seller, or simply a curious observer, our
+              commitment is to deliver accurate, up-to-date information to guide
+              you on your journey through the world of real estate. Join us as
+              we transform the way you perceive, assess, and navigate the
+              housing market, making your homeownership dreams a well-informed
+              reality. Welcome to Nestling – Where Homes and Insights Unite. At
+              Nestling, we believe in empowering individuals with the knowledge
+              to make informed decisions about one of life's most significant
+              investments – a home. Our platform specialises in extrapolating
+              private housing prices and conducting meticulous comparisons to
+              houses currently on the market. Whether you are a prospective
+              buyer, seller, or simply a curious observer, our commitment is to
+              deliver accurate, up-to-date information to guide you on your
+              journey through the world of real estate. Join us as we transform
+              the way you perceive, assess, and navigate the housing market,
+              making your homeownership dreams a well-informed reality. Welcome
+              to Nestling – Where Homes and Insights Unite.
+            </p>
           </div>
         </div>
       </div>
